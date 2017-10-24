fbc347a84bc56766671002f1c01e051c8f0b8631
Adrián Carro <adrian.carro@gmail.com>
2017-10-24 14:46:26 +0100

    Updating Construction class

--- src/main/java/housing/Construction.java
+++ src/main/java/housing/Construction.java
@@ -1,74 +1,97 @@
 package housing;
 
+import org.apache.commons.math3.random.MersenneTwister;
+
 import java.io.Serializable;
 import java.util.HashSet;
 
 
 public class Construction implements IHouseOwner, Serializable {
-	private static final long serialVersionUID = -6288390048595500248L;
+    private static final long serialVersionUID = -6288390048595500248L;
 
-	private Config	config = Model.config;	// Passes the Model's configuration parameters object to a private field
+    //------------------//
+    //----- Fields -----//
+    //------------------//
+
+    private int                         housingStock; // Total number of houses in the whole model
+    private int                         nNewBuild; // Number of houses built this month
+
+    private Config	                    config = Model.config; // Passes the Model's configuration parameters object to a private field
+    private MersenneTwister             rand = Model.rand; // Passes the Model's random number generator to a private field
+    private HashSet<House>              onMarket;
+
+    //------------------------//
+    //----- Constructors -----//
+    //------------------------//
 
 	public Construction() {
 		housingStock = 0;
 		onMarket = new HashSet<>();
-//		for(j = 0; j<Nh; ++j) { // setup houses
-//			houses[j] = new House();
-//			houses[j].quality = (int)(House.Config.N_QUALITY*j*1.0/Nh); // roughly same number of houses in each quality band
-//		}	
 	}
-	
+
+    //-------------------//
+    //----- Methods -----//
+    //-------------------//
+
 	public void init() {
-		housingStock = 0;
+        housingStock = 0;
 		onMarket.clear();
 	}
-	
+
 	public void step() {
-		int targetStock;
-		if(Model.households.size() < config.TARGET_POPULATION) {
-			targetStock = (int)(Model.households.size()*config.CONSTRUCTION_HOUSES_PER_HOUSEHOLD);
-		} else {
-			targetStock = (int)(config.TARGET_POPULATION*config.CONSTRUCTION_HOUSES_PER_HOUSEHOLD);
-		}
-		int shortFall = targetStock - housingStock;
-		House newBuild;
-		double price;
-		for(House h : onMarket) {
-			Model.houseSaleMarket.updateOffer(h.getSaleRecord(), h.getSaleRecord().getPrice()*0.95);
-		}
-		while(shortFall > 0) {
-			newBuild = new House();
-			newBuild.owner = this;
-			++housingStock;
-			price = Model.houseSaleMarket.referencePrice(newBuild.getQuality());
-//			if(Model.rand.nextDouble() < 0.9) {
-			Model.houseSaleMarket.offer(newBuild, price);
-			onMarket.add(newBuild);
-//			} else {
-//				Model.households.get(Model.rand.nextInt(Model.households.size())).inheritHouse(newBuild);
-//			}
-			--shortFall;
+        // Initialise to zero the number of houses built this month
+        nNewBuild = 0;
+        // First update prices of properties put on the market on previous time steps and still unsold
+        for(House h : onMarket) {
+            Model.houseSaleMarket.updateOffer(h.getSaleRecord(), h.getSaleRecord().getPrice()*0.95);
+        }
+        // Then, compute target housing stock dependent on current and target population
+        int targetStock;
+        if(Model.households.size() < config.TARGET_POPULATION) {
+            targetStock = (int)(Model.households.size()*config.CONSTRUCTION_HOUSES_PER_HOUSEHOLD);
+        } else {
+            targetStock = (int)(config.TARGET_POPULATION*config.CONSTRUCTION_HOUSES_PER_HOUSEHOLD);
+        }
+        // ...compute the shortfall of houses
+        int shortFall = targetStock - housingStock;
+        // ...if shortfall is positive...
+        if (shortFall > 0) {
+            // ...add this shortfall to the number of houses built this month
+            nNewBuild += shortFall;
+        }
+        // ...and while there is any positive shortfall...
+        House newHouse;
+        while(shortFall > 0) {
+            // ...create a new house with a random quality and with the construction sector as the owner
+            newHouse = new House((int)(rand.nextDouble()*config.N_QUALITY));
+            newHouse.owner = this;
+            // ...put the house for sale in the house sale market at the reference price for that quality
+            Model.houseSaleMarket.offer(newHouse,
+                    Model.housingMarketStats.getReferencePriceForQuality(newHouse.getQuality()));
+            // ...add the house to the portfolio of construction sector properties
+            onMarket.add(newHouse);
+            // ...and finally increase housing stocks, and decrease shortfall
+            ++housingStock;
+            --shortFall;
 		}
 	}
 
 	@Override
-	public void completeHouseSale(HouseSaleRecord sale) {
-		onMarket.remove(sale.house);
-	}
+	public void completeHouseSale(HouseSaleRecord sale) { onMarket.remove(sale.house); }
 
 	@Override
 	public void endOfLettingAgreement(House h, PaymentAgreement p) {
         System.out.println("Strange: a tenant is moving out of a house owned by the construction sector!");
-		System.exit(0);
-
 	}
 
 	@Override
 	public void completeHouseLet(HouseSaleRecord sale) {
         System.out.println("Strange: the construction sector is trying to let a house!");
-        System.exit(0);
 	}
 
-	public int housingStock;			// total number of houses built
-	HashSet<House> onMarket; 
+    //----- Getter/setter methods -----//
+
+    public int getHousingStock() { return housingStock; }
+
+    public int getnNewBuild() { return nNewBuild; }
 }
