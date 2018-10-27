c12a849302442582ba1aaa6adb108e6fdb0d6194
Adrián Carro <adrian.carro@gmail.com>
2018-10-27 17:02:33 +0100

    Implement quality-aware months-on-market averages
    This also cleans some residual days-on-market variables, rewiring everything to months-on-market equivalents.

--- src/main/java/collectors/RentalMarketStats.java
+++ src/main/java/collectors/RentalMarketStats.java
@@ -20,12 +20,7 @@ public class RentalMarketStats extends HousingMarketStats {
 	private HousingMarketStats  		housingMarketStats;
 	private Config                      config = Model.config; // Passes the Model's configuration parameters object to a private field
 
-	// Rental-specific variables computed during market clearing, counters
-	private double []                   sumMonthsOnMarketPerQualityCount; // Dummy counter
-
 	// Rental-specific variables computed after market clearing to keep the previous values during the clearing
-	private double []                   sumMonthsOnMarketPerQuality; // Sum of the months on market for each quality band for properties rented this month
-	private double []                   expAvMonthsOnMarketPerQuality; // Exponential moving average of the months on market for each quality band
 	private double []                   avOccupancyPerQuality; // Average fraction of time a rental property stays rented for each quality band
 	private double []                   avFlowYieldPerQuality; // Average gross rental yield for each quality band for properties rented out this month
 	private double                      avFlowYield; // Average gross rental yield for properties rented out this month
@@ -65,9 +60,6 @@ public class RentalMarketStats extends HousingMarketStats {
 	public void init() {
 		super.init();
 		// Set initial value for all rental specific variables
-		sumMonthsOnMarketPerQuality = new double[config.N_QUALITY];
-		expAvMonthsOnMarketPerQuality  = new double[config.N_QUALITY];
-		Arrays.fill(expAvMonthsOnMarketPerQuality, 1.0);
 		avOccupancyPerQuality = new double[config.N_QUALITY];
 		Arrays.fill(avOccupancyPerQuality, 1.0);
 		avFlowYieldPerQuality = new double[config.N_QUALITY];
@@ -75,34 +67,6 @@ public class RentalMarketStats extends HousingMarketStats {
 		avFlowYield = config.RENT_GROSS_YIELD;
 		expAvFlowYield = config.RENT_GROSS_YIELD;
 		longTermExpAvFlowYield = config.RENT_GROSS_YIELD;
-	}
-
-	//----- Rental-specific pre-market-clearing methods -----//
-
-	/**
-	 * This method extends the corresponding one at the HousingMarketStats class with some rental-specific variables
-	 * Computes pre-clearing statistics and resets counters to zero.
-	 */
-	@Override
-	public void preClearingRecord() {
-		super.preClearingRecord();
-		// Re-initialise to zero variables to be computed later on, during market clearing, counters
-		sumMonthsOnMarketPerQualityCount = new double[config.N_QUALITY];
-	}
-
-	//----- Rental-specific during-market-clearing methods -----//
-
-	/**
-	 * This method extends the corresponding one at the HousingMarketStats class with some rental-specific variables.
-     * Updates the values of several counters every time a buyer and a seller are matched and the transaction is
-	 * completed. Note that only counter variables can be modified within this method
-	 *
-	 * @param sale The HouseOfferRecord of the house being sold
-	 */
-	@Override
-	public void recordTransaction(HouseOfferRecord sale) {
-		super.recordTransaction(sale);
-		sumMonthsOnMarketPerQualityCount[sale.getQuality()] += (Model.getTime() - sale.gettInitialListing());
 	}
 
 	//----- Post-market-clearing methods -----//
@@ -114,20 +78,13 @@ public class RentalMarketStats extends HousingMarketStats {
 	@Override
 	public void postClearingRecord() {
 		super.postClearingRecord();
-		// Pass count value obtained during market clearing to persistent variables
-		System.arraycopy(sumMonthsOnMarketPerQualityCount, 0, sumMonthsOnMarketPerQuality, 0, config.N_QUALITY);
 		// Compute the rest of variables after market clearing...
 		double avFlowYieldCount = 0; // Dummy counter
 		for (int q = 0; q < config.N_QUALITY; q++) {
-			// ... exponential average of months in the market per quality band (only if there have been sales)
-			if (getnSalesForQuality(q) > 0) {
-				expAvMonthsOnMarketPerQuality[q] = config.derivedParams.E*expAvMonthsOnMarketPerQuality[q]
-						+ (1.0 - config.derivedParams.E)*sumMonthsOnMarketPerQuality[q]/getnSalesForQuality(q);
-			}
 			// ... average fraction of time that a house of a given quality is occupied, based on average tenancy length
 			// and exponential moving average of months that houses of this quality spend on the rental market
 			avOccupancyPerQuality[q] = config.TENANCY_LENGTH_AVERAGE/(config.TENANCY_LENGTH_AVERAGE
-					+ expAvMonthsOnMarketPerQuality[q]);
+					+ getExpAvMonthsOnMarketForQuality(q));
 			// ... average flow gross rental yield per quality band (stick to previous value if no sales)
 			if (housingMarketStats.getExpAvSalePriceForQuality(q) > 0) {
 				avFlowYieldPerQuality[q] = getExpAvSalePriceForQuality(q)*config.constants.MONTHS_IN_YEAR
@@ -151,10 +108,6 @@ public class RentalMarketStats extends HousingMarketStats {
 	// Note that, for security reasons, getters should never give or use counter variables, as their value changes
 	// during market clearing
 
-	public double [] getSumMonthsOnMarketPerQuality() { return sumMonthsOnMarketPerQuality; }
-	public double getSumMonthsOnMarketForQuality(int quality) { return sumMonthsOnMarketPerQuality[quality]; }
-	public double [] getExpAvMonthsOnMarketPerQuality() { return expAvMonthsOnMarketPerQuality; }
-	public double getExpAvMonthsOnMarketForQuality(int quality) { return expAvMonthsOnMarketPerQuality[quality]; }
 	public double [] getAvOccupancyPerQuality() { return avOccupancyPerQuality; }
 	public double getAvOccupancyForQuality(int quality) { return avOccupancyPerQuality[quality]; }
 	public double [] getAvFlowYieldPerQuality() { return avFlowYieldPerQuality; }
