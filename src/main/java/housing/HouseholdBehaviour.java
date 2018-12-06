a8eb93ad437981d4761f463a42654cb584ee0912
Ruben Tarne <ruben.tarne@gmail.com>
2018-12-06 16:29:40 +0100

    INET update - remove the cap on max BTL bid
    which was 10% over the maximum expected sale price. BTLbidprice in behaviour class has been removed

--- src/main/java/housing/HouseholdBehaviour.java
+++ src/main/java/housing/HouseholdBehaviour.java
@@ -608,16 +608,6 @@ public class HouseholdBehaviour {
         return bidOnTheHousingMarket;
     }
 
-    double btlPurchaseBid(Household me) {
-        // TODO: What is this 1.1 factor? Another fudge parameter? It prevents wealthy investors from offering more than
-        // TODO: 10% above the average price of top quality houses. The effect of this is to prevent fast increases of
-        // TODO: price as BTL investors buy all supply till prices are too high for everybody. Fairly unclear mechanism,
-        // TODO: check for removal!
-        return Model.bank.getMaxMortgage(me, false, false);
-//        return	(Math.min(Model.bank.getMaxMortgage(me, false, false),
-//                1.1*Model.housingMarketStats.getExpAvSalePriceForQuality(config.N_QUALITY-1)));
-    }
-
 	/**
 	 * How much rent does an investor decide to charge on a buy-to-let house? 
 	 * @param rbar exponential average rent for house of this quality
